c0f3bc553c6709757431a29e664bdba7b807b7fa
孙海洋 <3036851059@qq.com>
2026-05-30 17:40:37 +0800

    增添滑动延迟效果

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,28 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+
 import { DashboardPage } from "@/pages/dashboard-page";
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    const rafId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.destroy();
+    };
+  }, []);
+
   return <DashboardPage />;
 }
